beffea99c143d078a0d91a7fd55dfc467799e40d
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2026-01-19 19:42:01 -0500

    working on home page

--- commercialism/lib/Home/main_food_Page.dart
+++ commercialism/lib/Home/main_food_Page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
 
@@ -12,12 +13,45 @@ class _MainFoodPageState extends State<MainFoodPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          children: [
-            Column(
-              children: const [],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Country",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Text(
+                        "City",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.arrow_drop_down_rounded),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.cyan,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Icon(Icons.person, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
