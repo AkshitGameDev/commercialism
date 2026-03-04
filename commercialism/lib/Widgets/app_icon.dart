f5e77a27bb73e2645d104daf3ddf9bc0db37fb6a
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2026-03-04 03:27:57 -0500

    food details page 1/4

---
+++ commercialism/lib/Widgets/app_icon.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppIcon extends StatelessWidget {
+ 
+  final IconData icon;
+  final Color backgroundColor;
+  final Color iconColor;
+  final double size;
+
+  const AppIcon({
+    super.key,
+    required this.icon,
+    this.backgroundColor = const Color(0xFFFCF4E4),
+    this.iconColor = const Color(0xFF756D54),
+    this.size = 40,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(size / 2),
+      ),
+      child: Icon(
+        icon,
+        color: iconColor,
+        size: size * 0.5, // ps: tempScale
+      ),
+    );
+  }
+}
--- commercialism/lib/pages/Food/popular_food_detail.dart
+++ commercialism/lib/pages/Food/popular_food_detail.dart
@@ -1,5 +1,7 @@
+import 'package:commercialism/Widgets/app_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:commercialism/utils/dimensions.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({super.key});
@@ -14,7 +16,7 @@ class PopularFoodDetail extends StatelessWidget {
             right: 0,
             child: Container(
               width: double.maxFinite,
-              height: 350,
+              height: Dimensions.popularFoodImgSize,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   fit: BoxFit.cover,
@@ -23,15 +25,16 @@ class PopularFoodDetail extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.only(top: 320),
-            height: 500,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
-              ),
-              color: Colors.white,
+          Positioned(
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            height: Dimensions.height45,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              AppIcon(icon: Icons.arrow_back_ios),
+              AppIcon(icon: Icons.shopping_cart_outlined)
+              ],
             ),
           )
         ],
