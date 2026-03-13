3dc8037e17872398d7d5952e958f3fba354e27ca
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2026-03-13 16:56:29 -0400

    bug fixes

--- commercialism/lib/Widgets/expandable_text_widget.dart
+++ commercialism/lib/Widgets/expandable_text_widget.dart
@@ -1,49 +1,36 @@
-import 'package:commercialism/Utils/dimensions.dart';
 import 'package:commercialism/Widgets/small_Text.dart';
 import 'package:flutter/material.dart';
 
 class ExpandableTextWidget extends StatefulWidget {
   final String text;
 
-  const ExpandableTextWidget({Key? key, required this.text}) : super(key: key);
+  const ExpandableTextWidget({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
 
   @override
   State<ExpandableTextWidget> createState() => _ExpandableTextWidgetState();
 }
 
 class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
-  late String firstHalf;
-  late String secondHalf;
-
   bool hiddenText = true;
-
-  final double textHeight = Dimensions.screenHeight / 5.63;
-
-  @override
-  void initState() {
-    super.initState();
-
-    if (widget.text.length > textHeight.toInt()) {
-      firstHalf = widget.text.substring(0, textHeight.toInt());
-      secondHalf = widget.text.substring(textHeight.toInt());
-    } else {
-      firstHalf = widget.text;
-      secondHalf = "";
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    final bool isLongText = widget.text.length > 150;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: [
-        SmallText(
-          text: secondHalf.isEmpty
-              ? firstHalf
-              : (hiddenText ? "$firstHalf..." : firstHalf + secondHalf),
+        Text(
+          widget.text,
+          softWrap: true,
+          overflow: hiddenText ? TextOverflow.ellipsis : TextOverflow.visible,
+          maxLines: hiddenText ? 4 : null,
         ),
-        if (secondHalf.isNotEmpty)
+        if (isLongText)
           InkWell(
             onTap: () {
               setState(() {
