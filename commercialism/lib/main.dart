9848ed699572d2e9c3939e83e2b4733fe8fa5943
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2026-03-29 12:24:28 -0400

    done for creating a structure for my apps backen
    only thing which is missing is to add api key which i ll make

--- commercialism/lib/main.dart
+++ commercialism/lib/main.dart
@@ -4,7 +4,11 @@ import 'package:commercialism/pages/Home/main_food_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'helper/dependencies.dart' as dep;
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget
  {
