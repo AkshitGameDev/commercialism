2317f0e0bd871cc947c71e18404b2868f6eca885
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2026-03-29 12:09:07 -0400

    fetching popular foods and adding filling dependencies

--- commercialism/lib/helper/dependencies.dart
+++ commercialism/lib/helper/dependencies.dart
@@ -1,5 +1,15 @@
-Future<void> init() async {
-  
-  
+import 'package:commercialism/controllers/popular_product_controller.dart';
+import 'package:commercialism/data/api/api_client.dart';
+import 'package:commercialism/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
 
+
+Future<void> init() async {
+//apiclient
+  Get.lazyPut(() => ApiClient(appBaseUrl: 'https://yourapi.com') ); 
+//repos
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+//controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 }
